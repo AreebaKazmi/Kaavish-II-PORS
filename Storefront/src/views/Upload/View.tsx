e1c179aab07cb26502cf989d699000380d50fd16
tasneem-98 <ta02903@st.habib.edu.pk>
2020-06-27 02:13:40 +0500

    added upload images with recommendations

--- Storefront/src/views/Upload/View.tsx
+++ Storefront/src/views/Upload/View.tsx
@@ -19,23 +19,25 @@ export const View: React.FC<ViewProps> = ({ }) => {
         <h1 className="upload__header"> Some of the recommendations from previously uploaded images are as follows: </h1>
 
       </div>
-      <div> 
-        {RecommendationData.map((rcm, index) =>{
-          return <div>
-            <h5> {rcm.ID} </h5>
-            <p> {rcm.upload_path} </p>
-          </div>
-        })}
-      </div>
+
       <div>
-        {RecommendationData.map((rcm, index) =>{
+        {RecommendationData.map((rcm, index) => {
           return <div>
-          <img src={require(`${rcm.upload_path}`)} width="250" height="300" />
+            <h3> {rcm.ID} </h3>
+            <img src={require(`${rcm.upload_path}`)} width="250" height="300" />
+            <ul>
+              {rcm.recommended.map((sub, index) => {
+              return <div className = "upload__right">
+                <ul>
+                  <img src={require(`${sub.path}`)} width="150" height="200" />
+                </ul>
+              </div>
+              })}
+            </ul>
           </div>
         })}
-
       </div>
-
+      
     </>
   );
 };
